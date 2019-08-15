5bf0fd8a6e60c425adda2faf642401844ddb8238
Ola Vea <ola@olavea.com>
2019-08-15 13:05:46 +0200

    initial commit see issue #1 for details

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/data/`,
+      },
+    },
+    `gatsby-transformer-csv`,
+  ],
+}
