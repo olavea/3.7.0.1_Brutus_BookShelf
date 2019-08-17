82c68960ee11f3dce4534c88e34f9fe36d4a74e5
Ola Vea <ola@olavea.com>
2019-08-17 14:21:18 +0200

    chips and props #3

--- src/components/bookCard.js
+++ src/components/bookCard.js
@@ -7,15 +7,19 @@ import CardContent from "@material-ui/core/CardContent"
 import CardMedia from "@material-ui/core/CardMedia"
 import Button from "@material-ui/core/Button"
 import Typography from "@material-ui/core/Typography"
+import Chip from "@material-ui/core/Chip"
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   card: {
     maxWidth: 345,
   },
   media: {
     height: 140,
   },
-})
+  chip: {
+    margin: theme.spacing(1),
+  },
+}))
 
 export default function BookCard({ book }) {
   const classes = useStyles()
@@ -33,8 +37,12 @@ export default function BookCard({ book }) {
             {book.Norsk_navn}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            {book.Engelsk_navn}–{book.Enhj_rninger_eller_kaniner}–
-            {book.Type_bok}
+            <Chip label={book.Engelsk_navn} className={classes.chip} />
+            <Chip
+              label={book.Enhj_rninger_eller_kaniner}
+              className={classes.chip}
+            />
+            <Chip label={book.Type_bok} className={classes.chip} />
           </Typography>
         </CardContent>
       </CardActionArea>
