9a430453700ccf497956314fa4a750af3ab618ef
Ola Vea <ola@olavea.com>
2020-01-29 22:20:43 +0100

    added useAuth #18

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,16 @@ import { useAuth } from "react-use-auth";
 const NotFoundPage = () => {
   const { isAuthenticated, login, logout } = useAuth();
   if (isAuthenticated()) {
-    return <Button onClick={logout}>Logout</Button>;
+    return (
+      <Button
+        onClick={logout}
+        variant="contained"
+        color="secondary"
+        size="large"
+      >
+        Logout
+      </Button>
+    );
   } else {
     return <Button onClick={login}>Login</Button>;
   }
